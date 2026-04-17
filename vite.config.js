cc6e9a75d80bc76f2c05f6c02d182e2696dab5ff
Alunisme <413730267@o365.tku.edu.tw>
2026-04-17 11:54:33 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/20260417/',   // ← 加上這行，填你的倉庫名稱
+  base: '/202604171/',   // ← 加上這行，填你的倉庫名稱
 })
